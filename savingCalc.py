980d316672b62c0d98f3e36558ba91b25ecdb9e7
jaithind <jaideepthind99@gmail.com>
2021-03-01 21:02:22 -0600

    Functionality added
    Calculator and its tests

---
+++ savingCalc.py
@@ -0,0 +1,12 @@
+def savingCalc(curAge, salary, perSaved, desGoal):
+    curAge = float(curAge)
+    salary = float(salary)
+    perSaved = float(perSaved)
+    desGoal = float(desGoal)
+    
+    saved = (perSaved/100) * salary
+    saved += (35/100) * saved
+    goalAge = (desGoal/saved) + curAge
+    goalAge = round(goalAge)
+    
+    return goalAge
