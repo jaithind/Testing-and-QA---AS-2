0d9dac341b180a2e6c314a8912f6bd660fc76138
jaithind <jaideepthind99@gmail.com>
2021-03-01 20:57:22 -0600

    Deletions

--- savingCalc_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from savingCalc import savingCalc
-
-class TestCase(unittest.TestCase):
-
-    def test_savingCalc(self):
-        result = savingCalc(25, 75000, 15, 100000)
-        self.assertEqual(result, 32)
